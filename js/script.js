e32d43b1e7be6f2eee668e0a145ea25ad0ec3102
tonyzoer <okamanahi@gmail.com>
2017-06-25 20:30:51 +0300

    .well->eq-height

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
-    var heights = $(".well").map(function() {
+    var heights = $(".eq-height").map(function() {
         return $(this).height();
     }).get(),
 
     maxHeight = Math.max.apply(null, heights);
 
-    $(".well").height(maxHeight);
+    $(".eq-height").height(maxHeight);
 });
